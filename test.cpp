d7b970c791cff96286d371a9718be496d2a6d5ff
jb <s7515199@gmail.com>
2015-05-12 23:03:44 +0900

    testing git first

---
+++ test.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class calcul{
+private :
+
+public :
+	void display(){
+		cout<<"1.plus"<<endl;
+		cout<<"2.minus"<<endl;
+		cout<<"what is your option? :";
+	}
+
+	void add(int a,int b){
+		cout<<"a+b="<<a+b<<endl;
+	}
+};
+
+int main()
+{
+	int a,b;
+	int option;
+	calcul calculator;
+	cout<<"input two numbers : "; cin>>a>>b;
+	calculator.display(); cin>>option;
+	if(option==1)
+		calculator.add(a,b);
+	else
+		cout<<"ㄲㅈ"<<endl;
+	return 0;
+}
