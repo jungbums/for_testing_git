0bfe14566966e8d980d6dca3257ec4edeadb9ef7
jb <s7515199@gmail.com>
2015-05-31 14:54:39 +0900

    add x, /

--- test.cpp
+++ test.cpp
@@ -8,6 +8,8 @@ public :
 	void display(){
 		cout<<"1.plus"<<endl;
 		cout<<"2.minus"<<endl;
+		cout<<"3.nanugi"<<endl;
+		cout<<"4.gobhagi"<<endl;
 		cout<<"what is your option? :";
 	}
 
@@ -16,6 +18,17 @@ public :
 	}
 	void minus(int a,int b){
 		cout<<"a-b="<<a-b<<endl;
+	}
+	void nanugi(int a, int b){
+		if(b==0){
+			cout<<"0으로 나눌 수 없음!"<<endl;
+		}
+		else{
+			cout<<"a/b="<<(double)a/b<<endl;
+		}
+	}
+	void gobhagi(int a, int b){
+		cout<<"axb="<<a*b<<endl;
 	}
 };
 
@@ -30,6 +43,10 @@ int main()
 		calculator.add(a,b);
 	else if(option==2)
 		calculator.minus(a,b);
+	else if(option==3)
+		calculator.nanugi(a,b);
+	else if(option==4)
+		calculator.gobhagi(a,b);
 	else
 		cout<<"ㄲㅈ"<<endl;
 	return 0;
